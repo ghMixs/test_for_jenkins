fd4ced2202b40a5433837830ac19971f0ed1014c
ghMixs <92264322+ghMixs@users.noreply.github.com>
2023-05-17 16:52:11 +0300

    Create qwe.py

---
+++ qwe.py
@@ -0,0 +1,9 @@
+from flask import Flask
+app = Flask(__name__)
+
+@app.route('/api/testing')
+def get_last_transaction():
+    return "<h1>Test</h1>"
+
+if __name__ == '__main__':
+    app.run(port=80)
