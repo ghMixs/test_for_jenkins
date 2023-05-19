08fa2986f424e3e7eaa9cb7498183f5cbe5e1875
ghMixs <92264322+ghMixs@users.noreply.github.com>
2023-05-19 15:02:28 +0300

    Update qwe.py

--- qwe.py
+++ qwe.py
@@ -3,7 +3,7 @@ app = Flask(__name__)
 
 @app.route('/api/testing')
 def get_last_transaction():
-    return "<h1>sss</h1>"
+    return "<h1>ssss</h1>"
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0')
